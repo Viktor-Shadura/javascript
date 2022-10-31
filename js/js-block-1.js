52f1b24289dbca384cd1263348a7b6b1fc6e5507
Viktor Shadura <shaduravik@gmail.com>
2022-11-01 01:17:00 +0200

    task 1.11

--- js/js-block-1.js
+++ js/js-block-1.js
@@ -238,22 +238,22 @@
 // Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
 
 
-function add(a, b, c) {
-  // Change code below this line
+// function add(a, b, c) {
+//   // Change code below this line
+// return a + b + c;
 
+//   // Change code above this line
+// }
 
+// add(2, 5, 8); // 15
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
 
-  // Change code above this line
-}
-
-add(2, 5, 8); // 15
-
-console.log(add(15, 27, 10));
-console.log(add(10, 20, 30));
-console.log(add(5, 10, 15));
-
-
-
+// console.log(add(2, 5, 8));
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
 
 
 /**
@@ -261,6 +261,36 @@ console.log(add(5, 10, 15));
   | TASK # 10
   |============================
 */
+
+// Функція makeMessage(name, price) складає і повертає повідомлення про покупку. Вона оголошує два параметри, значення яких будуть задаватися під час її виклику.
+
+// name - назва товару
+// price - ціна товару
+// Доповни код функції таким чином, щоб у змінну message записувався рядок "You picked <product name>, price per item is <product price> credits", де <product name> і <product price> - це значення параметрів name і price. Використовуй синтаксис шаблонних рядків.
+
+// Увага
+// Зверни увагу на те, що в коді відсутній виклик функції makeMessage. З цього завдання і далі ми самі будемо викликати твої функції і перевіряти те, як вони працюють. Результат наших перевірок ти побачиш у блоці Результати під редактором коду.
+
+// Оголошена функція makeMessage(name, price)
+// Виклик makeMessage('Radar', 6150) повертає "You picked Radar, price per item is 6150 credits"
+// Виклик makeMessage('Scanner', 3500) повертає "You picked Scanner, price per item is 3500 credits"
+// Виклик makeMessage('Reactor', 8000) повертає "You picked Reactor, price per item is 8000 credits"
+// Виклик makeMessage('Engine', 4070) повертає "You picked Engine, price per item is 4070 credits"
+
+
+// function makeMessage (name, price) {
+//   // Change code below this line
+//    const message = `You picked ${name}, price per item is ${price} credits`;
+//   // Change code above this line
+//   return message;
+// };
+
+// console.log(makeMessage ('Radar', 6150));
+// console.log(makeMessage ('Scanner', 3500));
+// console.log(makeMessage ('Reactor', 8000));
+// console.log(makeMessage ('Engine', 4070));
+
+
 /**
   |============================
   | TASK # 11

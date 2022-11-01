3492c3916be48d25765d787ec17545d3d309a35b
Viktor Shadura <shaduravik@gmail.com>
2022-11-02 01:58:22 +0200

    work task 1.15

--- js/js-block-1.js
+++ js/js-block-1.js
@@ -296,26 +296,171 @@
   | TASK # 11
   |============================
 */
+
+// Функція calculateTotalPrice рахує і повертає загальну суму покупки. Вона приймає два параметри, значення яких будуть задаватися під час її виклику.
+
+// orderedQuantity - кількість одиниць товару в замовленні;
+// pricePerItem - ціна однієї одиниці товару.
+// Доповни код функції таким чином, щоб у змінну totalPrice записувалася загальна сума покупки - результат множення кількості товарів на ціну одного.
+
+// Оголошена функція calculateTotalPrice (orderedQuantity, pricePerItem)
+// Виклик calculateTotalPrice(5, 100) повертає 500
+// Виклик calculateTotalPrice(8, 60) повертає 480
+// Виклик calculateTotalPrice(3, 400) повертає 1200
+// Виклик calculateTotalPrice(1, 3500) повертає 3500
+// Виклик calculateTotalPrice(12, 70) повертає 840
+// Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+
+
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//   // Change code below this line
+//   const totalPrice = (orderedQuantity * pricePerItem);
+
+//   // Change code above this line
+//   return totalPrice;
+// };
+
+// console.log(calculateTotalPrice(5, 100));
+// console.log(calculateTotalPrice(8, 60));
+// console.log(calculateTotalPrice(3, 400));
+// console.log(calculateTotalPrice(1, 3500));
+// console.log(calculateTotalPrice(12, 70));
+
+
 /**
   |============================
   | TASK # 12
   |============================
 */
+
+// Функція makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) складає і повертає повідомлення про покупку ремонтних дроїдів. Вона оголошує три параметри, значення яких будуть задаватися під час її виклику.
+
+// orderedQuantity - кількість дроїдів у замовленні
+// pricePerDroid - ціна одного дроїда
+// deliveryFee - вартість доставки
+// Доповни код функції таким чином, щоб вона повертала повідомлення про замовлення у форматі "You ordered droids worth <total price> credits. Delivery (<delivery fee> credits) is included in total price.". Не забудь про ціну доставки в обчисленнях загальної вартості.
+
+// Оголошена функція makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee)
+// Виклик makeOrderMessage(2, 100, 50) повертає "You ordered droids worth 250 credits. Delivery (50 credits) is included in total price."
+// Виклик makeOrderMessage(4, 300, 100) повертає "You ordered droids worth 1300 credits. Delivery (100 credits) is included in total price."
+// Виклик makeOrderMessage(10, 70, 200) повертає "You ordered droids worth 900 credits. Delivery (200 credits) is included in total price."
+
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   // Change code below this line
+//   const totalPrice = (orderedQuantity * pricePerDroid + deliveryFee);
+
+//   // Change code above this line
+//   return message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// }
+
+// console.log(makeOrderMessage(2, 100, 50));
+// console.log(makeOrderMessage(4, 300, 100));
+// console.log(makeOrderMessage(10, 70, 200));
+
+
 /**
   |============================
   | TASK # 13
   |============================
 */
+
+// Функція isAdult оголошує один параметр age (вік), значення якого буде задаватися під час її виклику. Присвой змінній passed вираз перевірки віку користувача на повноліття. Людина вважається повнолітньою у віці 18 років і старше.
+
+// Оголошена функція isAdult(age)
+// У виразі перевірки використовується оператор >=
+// Виклик isAdult(20) повертає true
+// Виклик isAdult(14) повертає false
+// Виклик isAdult(8) повертає false
+// Виклик isAdult(37) повертає true
+
+
+// function isAdult(age) {
+//   // Change code below this line
+//   const passed = (age >= 18);
+
+//   // Change code above this line
+//   return passed;
+// }
+
+// console.log(isAdult(20));
+// console.log(isAdult(14));
+// console.log(isAdult(8));
+// console.log(isAdult(37));
+
+
 /**
   |============================
   | TASK # 14
   |============================
 */
+
+// Функція isValidPassword (password) перевіряє рівність збереженого і введеного паролів і повертає результат перевірки - буль true або false. Змінна SAVED_PASSWORD зберігає значення попередньо збереженого пароля. Введений пароль передається у параметр password.
+
+// Присвой змінній isMatch вираз перевірки рівності введених і попередньо збережених паролів. Результатом виразу перевірки повинно бути true, якщо значення збігаються, і false - якщо ні.
+
+// Оголошена функція isValidPassword(password)
+// У виразі перевірки паролів використаний оператор ===
+// Виклик isValidPassword("mangodab3st") повертає false
+// Виклик isValidPassword("kiwirul3z") повертає false
+// Виклик isValidPassword("jqueryismyjam") повертає true
+
+
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = 'jqueryismyjam';
+//   // Change code below this line
+//   const isMatch = (SAVED_PASSWORD === password);
+
+//   // Change code above this line
+//   return isMatch;
+// }
+
+// console.log(isValidPassword("mangodab3st"));
+// console.log(isValidPassword("kiwirul3z"));
+// console.log(isValidPassword("jqueryismyjam"));
+
+
 /**
   |============================
   | TASK # 15
   |============================
 */
+
+// Додай вираз перевірки повноліття користувача, значення параметра age, в умову для інструкції if.
+
+// Якщо користувач повнолітній, повинен виконуватися блок if і у змінну message записується рядок "You are an adult".
+// В іншому випадку повинен виконуватися блок else і записується рядок "You are a minor".
+// Оголошена функція checkAge(age).
+// У виразі перевірки віку використаний оператор >=
+// Виклик checkAge(20) повертає "You are an adult"
+// Виклик checkAge(8) повертає "You are a minor"
+// Виклик checkAge(14) повертає "You are a minor"
+// Виклик checkAge(38) повертає "You are an adult"
+
+// function checkAge(age) {
+//   let message;
+
+//   if () { // Change this line
+//     message = 'You are an adult';
+//   } else {
+//     message = 'You are a minor';
+//   }
+
+//   return message;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
   |============================
   | TASK # 16

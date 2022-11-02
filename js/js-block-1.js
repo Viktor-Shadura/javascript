14c8e8d4504d1c6bf8546cd5b60a8fb1db3ebe58
Viktor Shadura <shaduravik@gmail.com>
2022-11-03 01:21:58 +0200

    task 1.21

--- js/js-block-1.js
+++ js/js-block-1.js
@@ -440,7 +440,7 @@
 // function checkAge(age) {
 //   let message;
 
-//   if () { // Change this line
+//   if (age>= 18) { // Change this line
 //     message = 'You are an adult';
 //   } else {
 //     message = 'You are a minor';
@@ -449,16 +449,10 @@
 //   return message;
 // }
 
-
-
-
-
-
-
-
-
-
-
+// console.log(checkAge(20));
+// console.log(checkAge(8));
+// console.log(checkAge(14));
+// console.log(checkAge(38));
 
 
 /**
@@ -466,31 +460,267 @@
   | TASK # 16
   |============================
 */
+
+// Функція checkStorage(available, ordered) перевіряє можливість оформлення замовлення і повертає повідомлення про результат. Вона оголошує два параметри, значення яких будуть задаватися під час її виклику:
+
+// available - загальна кількість товарів на складі
+// ordered - одиниць товару в замовленні
+// Використовуючи розгалуження, доповни код функції таким чином, що:
+
+// Якщо в замовленні вказане число, яке перевищує кількість товарів на складі, у змінну message записується рядок "Not enough goods in stock!".
+// В іншому випадку записується рядок "Order is processed, our manager will contact you.".
+// Оголошена функція checkStorage(available, ordered).
+// Виклик checkStorage(100, 50) повертає "Order is processed, our manager will contact you."
+// Виклик checkStorage(100, 130) повертає "Not enough goods in stock!"
+// Виклик checkStorage(200, 20) повертає "Order is processed, our manager will contact you."
+// Виклик checkStorage(200, 150) повертає "Order is processed, our manager will contact you."
+// Виклик checkStorage(150, 180) повертає "Not enough goods in stock!"
+
+
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+
+//     if (available >= ordered) { // Change this line
+//       message = 'Order is processed, our manager will contact you.';
+//     } else {
+//       message = 'Not enough goods in stock!';
+//     }
+
+//   // Change code above this line
+//   return message;
+// }
+
+//   console.log(checkStorage(100, 50));
+//   console.log(checkStorage(100, 130));
+//   console.log(checkStorage(200, 20));
+//   console.log(checkStorage(200, 150));
+//   console.log(checkStorage(150, 180));
+
+
 /**
   |============================
   | TASK # 17
   |============================
 */
+
+// Заміни вирази зі стандартними математичними операторами на комбінований оператор присвоєння з додаванням, відніманням, множенням і діленням.
+
+// Значення змінної a дорівнює 7
+// Використаний оператор +=
+// Значення змінної b дорівнює 6
+// Використаний оператор -=
+// Значення змінної c дорівнює 45
+// Використаний оператор *=
+// Значення змінної d дорівнює 2
+// Використаний оператор /=
+
+
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+
+// // Change code below this line
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+
 /**
   |============================
   | TASK # 18
   |============================
 */
+
+// Станція з продажу ремонтних дроїдів готова до запуску, залишилося написати програмне забезпечення для відділу продажів.
+
+// Функція makeTransaction(pricePerDroid, orderedQuantity, customerCredits) виконує транзакцію з продажу дроїдів і повертає повідомлення про результат операції. Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
+
+// pricePerDroid - ціна одного дроїда
+// orderedQuantity - кіл-сть замовлених дроїдів
+// customerCredits - сума коштів на рахунку клієнта
+// Доповни її наступним функціоналом:
+
+// Оголоси змінну totalPrice для зберігання загальної суми замовлення і присвой їй вираз розрахунку цієї суми.
+// Додай перевірку, чи зможе клієнт оплатити замовлення:
+// якщо сума до сплати перевищує кількість кредитів на рахунку клієнта, запиши у змінну message рядок "Insufficient funds!";
+// в іншому випадку, відніми суму покупки з рахунку клієнта і запиши у змінну message повідомлення: "You ordered <число> droids, you have <число> credits left".
+// Оголошена функція makeTransaction(pricePerDroid, orderedQuantity, customerCredits)
+// Виклик makeTransaction(3000, 5, 23000) повертає "You ordered 5 droids, you have 8000 credits left"
+// Виклик makeTransaction(1000, 3, 15000) повертає "You ordered 3 droids, you have 12000 credits left"
+// Виклик makeTransaction(5000, 10, 8000) повертає "Insufficient funds!"
+// Виклик makeTransaction(2000, 8, 10000) повертає "Insufficient funds!"
+// Виклик makeTransaction(500, 10, 5000) повертає "You ordered 10 droids, you have 0 credits left"
+
+
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   // Change code below this line
+//   const totalPrice = (pricePerDroid * orderedQuantity);
+
+//   if (customerCredits >= totalPrice) {
+//     message = `You ordered ${orderedQuantity} droids, you have ${customerCredits - totalPrice} credits left`;
+//   } else {
+//     message = 'Insufficient funds!';
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+
+// console.log(makeTransaction(3000, 5, 23000));
+// console.log(makeTransaction(1000, 3, 15000));
+// console.log(makeTransaction(5000, 10, 8000));
+// console.log(makeTransaction(2000, 8, 10000));
+// console.log(makeTransaction(500, 10, 5000));
+
+
 /**
   |============================
   | TASK # 19
   |============================
 */
+
+// Функція checkPassword(password) отримує пароль користувача в параметр password, перевіряє його на збіг з паролем адміністратора у змінній ADMIN_PASSWORD і повертає повідомлення про результат порівняння, яке зберігається у змінній message.
+
+// Якщо значення параметра password дорівнює null, значить користувач скасував операцію і в message записується рядок "Canceled by user!".
+// Якщо значення параметра password збігається зі значенням ADMIN_PASSWORD, у змінну message присвоюється рядок "Welcome!".
+// Якщо жодна з попередніх умов не виконалася, у змінну message записується рядок "Access denied, wrong password!".
+// Оголошена функція checkPassword(password)
+// Виклик checkPassword("mangohackzor") повертає "Access denied, wrong password!"
+// Виклик checkPassword(null) повертає "Canceled by user!"
+// Виклик checkPassword("polyhax") повертає "Access denied, wrong password!"
+// Виклик checkPassword("jqueryismyjam") повертає "Welcome!"
+
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
+
+//   if (password === "null") { // Change this line
+//     message =  'Canceled by user!';
+//   } else if (password === ADMIN_PASSWORD) { // Change this line
+//     message = 'Welcome!';
+//   } else {
+//     message = 'Access denied, wrong password!';
+//   }
+
+//   return message;
+// }
+
+// console.log(checkPassword("mangohackzor"));
+// console.log(checkPassword("null"));
+// console.log(checkPassword("polyhax"));
+// console.log(checkPassword("jqueryismyjam"));
+
+
 /**
   |============================
   | TASK # 20
   |============================
 */
+
+// Функція checkStorage(available, ordered) перевіряє можливість оформлення замовлення і повертає повідомлення про результат. Вона оголошує два параметри, значення яких будуть задаватися під час її виклику.
+
+// available - доступна кількість товарів на складі
+// ordered - одиниць товару в замовленні
+// Використовуючи розгалуження, доповни код функції таким чином, що:
+
+// Якщо в замовленні ще немає товарів, тобто значення параметра ordered дорівнює 0, у змінну message присвоюється рядок "There are no products in the order!".
+// Якщо товарів у замовленні більше, ніж доступно товарів на складі, то у змінну message присвоюється рядок "Your order is too large, there are not enough items in stock!".
+// В іншому випадку у змінну message присвоюється рядок "The order is accepted, our manager will contact you".
+// Оголошена функція checkStorage(available, ordered)
+// Виклик checkStorage(100, 50) повертає "The order is accepted, our manager will contact you"
+// Виклик checkStorage(100, 130) повертає "Your order is too large, there are not enough items in stock!"
+// Виклик checkStorage(70, 0)повертає "There are no products in the order!"
+// Виклик checkStorage(200, 20) повертає "The order is accepted, our manager will contact you"
+// Виклик checkStorage(200, 250) повертає "Your order is too large, there are not enough items in stock!"
+// Виклик checkStorage(150, 0) повертає "There are no products in the order!"
+
+
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+  
+//   if (ordered === 0) { // Change this line
+//     message =  'There are no products in the order!';
+//   } else if (ordered > available) { // Change this line
+//     message = 'Your order is too large, there are not enough items in stock!';
+//   } else {
+//     message = 'The order is accepted, our manager will contact you';
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(70, 0));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 250));
+// console.log(checkStorage(150, 0));
+
+
 /**
   |============================
   | TASK # 21
   |============================
 */
+
+// Функція isNumberInRange(start, end, number) перевіряє, чи входить число у проміжок. Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
+
+// number - число, входження якого перевіряється
+// start - початок числового проміжку
+// end - кінець числового проміжку
+// Присвой змінній isInRange вираз перевірки входження number у числовий проміжок від start до end. Тобто число повинно бути більшим або дорівнювати start, і меншим або дорівнювати end. Результатом виразу перевірки буде буль true або false.
+
+// Оголошена функція isNumberInRange(start, end, number)
+// У виразі перевірки використаний оператор &&
+// Виклик isNumberInRange(10, 30, 17) повертає true
+// Виклик isNumberInRange(10, 30, 5) повертає false
+// Виклик isNumberInRange(20, 50, 24) повертає true
+// Виклик isNumberInRange(20, 50, 76) повертає false
+
+
+function isNumberInRange(start, end, number) {
+  const isInRange = ; // Change this line
+
+  return isInRange;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
   |============================
   | TASK # 22

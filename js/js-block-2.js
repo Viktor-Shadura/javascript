61c25bfa7a68e12e9230f70248a2901668ef2801
Viktor Shadura <shaduravik@gmail.com>
2022-11-10 01:32:16 +0200

    task 2.3

--- js/js-block-2.js
+++ js/js-block-2.js
@@ -3,16 +3,113 @@
   | TASK # 1
   |============================
 */
+
+// Запиши умову в інструкції if таким чином, щоб функція працювала правильно.
+
+// Оголошена функція checkAge(age).
+// У виразі перевірки віку використаний оператор >=
+// Виклик checkAge(20) повертає "You are an adult"
+// Виклик checkAge(8) повертає "You are a minor"
+// Виклик checkAge(14) повертає "You are a minor"
+// Виклик checkAge(38) повертає "You are an adult"
+// В тілі функції є тільки одна інструкція if
+// В тілі функції відсутні інструкції else або else if
+
+
+// function checkAge(age) {
+//   if (age >= 18) { // Change this line
+//     return "You are an adult";
+//   }
+
+//   return "You are a minor";
+// }
+
+// console.log(checkAge(20));
+// console.log(checkAge(8));
+// console.log(checkAge(14));
+// console.log(checkAge(38));
+
+
 /**
   |============================
   | TASK # 2
   |============================
 */
+
+// Функція checkPassword отримує пароль користувача у параметр password, перевіряє його на збіг з паролем адміністратора у змінній ADMIN_PASSWORD і повертає повідомлення про результат порівняння.
+
+// Виконай рефакторинг коду функції checkPassword, використовуючи патерн «раннє повернення»:
+
+// видали змінну message
+// видали else
+// код повинен працювати так само, як і до оптимізації
+// Оголошена функція checkPassword(password)
+// Виклик checkPassword("mangohackzor") повертає "Access denied, wrong password!"
+// Виклик checkPassword("polyhax") повертає "Access denied, wrong password!"
+// Виклик checkPassword("jqueryismyjam") повертає "Welcome!"
+
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   // Change code below this line
+
+//   if (password === ADMIN_PASSWORD) { // Change this line
+//     return "Welcome!";
+//   }
+
+//   return "Access denied, wrong password!";
+// }
+
+// console.log(checkPassword("mangohackzor"));
+// console.log(checkPassword("polyhax"));
+// console.log(checkPassword("jqueryismyjam"));
+
+
 /**
   |============================
   | TASK # 3
   |============================
 */
+
+// Функція checkStorage перевіряє можливість оформлення замовлення і повертає повідомлення про результат. Вона приймає два параметри, значення яких будуть задаватися під час її виклику.
+
+// available - доступна кількість товарів на складі
+// ordered - кількість одиниць товару в замовленні
+// Виконай рефакторинг коду функції checkStorage, використовуючи патерн «раннє повернення».
+
+// Оголошена функція checkStorage(available, ordered)
+// Виклик checkStorage(100, 50) повертає "The order is accepted, our manager will contact you"
+// Виклик checkStorage(100, 130) повертає "Your order is too large, not enough goods in stock!"
+// Виклик checkStorage(70, 0) повертає "Your order is empty!"
+// Виклик checkStorage(200, 20) повертає "The order is accepted, our manager will contact you"
+// Виклик checkStorage(200, 250) повертає "Your order is too large, not enough goods in stock!"
+// Виклик checkStorage(150, 0) повертає "Your order is empty!"
+
+
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+//   let message;
+
+//   if (ordered === 0) {
+//     message = "Your order is empty!";
+//   } else if (ordered > available) {
+//     message = "Your order is too large, not enough goods in stock!";
+//   } else {
+//     message = "The order is accepted, our manager will contact you";
+//   }
+
+//   return message;
+//   // Change code above this line
+// }
+
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 50));
+
+
 /**
   |============================
   | TASK # 4

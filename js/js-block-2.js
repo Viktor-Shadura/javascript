9f07b8d0bb02d8c1f68352e3a843918a34fd0575
Viktor Shadura <shaduravik@gmail.com>
2022-12-05 01:39:22 +0200

    task 2.32

--- js/js-block-2.js
+++ js/js-block-2.js
@@ -639,18 +639,20 @@
 // В циклі for використовувався метод push
 
 
-function createArrayOfNumbers(min, max) {
-  const numbers = [];
-  // Change code below this line
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   // Change code below this line
+//   for (let i = min; i <= max; i += 1) {
+//     numbers.push(i);
+//     min += 1
+//     }
+//   // Change code above this line
+//   return numbers;
+// }
 
-  // Change code above this line
-  return numbers;
-}
-
-
-console.log(createArrayOfNumbers(1, 3));
-console.log(createArrayOfNumbers(14, 17));
-console.log(createArrayOfNumbers(29, 34));
+// console.log(createArrayOfNumbers(1, 3));
+// console.log(createArrayOfNumbers(14, 17));
+// console.log(createArrayOfNumbers(29, 34));
 
 
 /**
@@ -658,48 +660,274 @@ console.log(createArrayOfNumbers(29, 34));
   | TASK # 23
   |============================
 */
+
+// Напиши функцію filterArray(numbers, value), яка приймає масив чисел (параметр numbers) і повертає новий масив, в якому будуть тільки ті елементи масиву numbers, які більші за значення параметра value (число).
+
+
+// function filterArray(numbers, value) {
+//    // Change code below this line
+//   let newNumbers = [];
+//    for (const number of numbers) {
+//      if (number > value){
+//      newNumbers.push(number);
+//      }
+//    }
+//   return newNumbers;
+//   // Change code above this line
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+
 /**
   |============================
   | TASK # 24
   |============================
 */
+
+// Функція checkFruit(fruit) приймає рядок з назвою фрукта (параметр fruit), і перевіряє, чи присутній такий фрукт в масиві fruits.
+
+// Доповни код функції таким чином, що якщо:
+
+// фрукт присутній в масиві, то функція повертає true;
+// фрукт відсутній в масиві, то функція повертає false.
+
+
+// function checkFruit(fruit) {
+//   const fruits = ["apple", "plum", "pear", "orange"];
+
+//   return fruits.includes(fruit); // Change this line
+// }
+
+// console.log(checkFruit("plum"));
+// console.log(checkFruit("mandarin"));
+// console.log(checkFruit("pear"));
+// console.log(checkFruit("Pear"));
+// console.log(checkFruit("apple"));
+
+
 /**
   |============================
   | TASK # 25
   |============================
 */
+
+// Спільними елементами масивів називають ті елементи, які присутні у всіх масивах.
+
+// Наприклад, у двох масивах [1, 3, 5] і [0, 8, 5, 3] спільними будуть числа 3 і 5, оскільки вони присутні в обох вихідних масивах. А числа 0, 1 і 8 присутні тільки в одному з масивів.
+
+// Напиши функцію getCommonElements(array1, array2), яка отримує два масиви довільної довжини в параметри array1 і array2, і повертає новий масив, що складається з тих елементів, які присутні в обох вихідних масивах.
+
+
+// function getCommonElements(array1, array2) {
+//   // Change code below this line
+//   const arrayFinish = [];
+
+//   for (const number of array1) {
+//     if (array2.includes(number)) {
+//       arrayFinish.push(number);
+//     }
+      
+//   }
+
+//   return arrayFinish;
+//  // Change code above this line
+// }
+
+// console.log(getCommonElements([1, 2, 3], [2, 4]));
+// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+// console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
+
+
 /**
   |============================
   | TASK # 26
   |============================
 */
+
+// Виконай рефакторинг коду функції calculateTotalPrice(order), замінивши цикл for на for...of.
+
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+
+//   for (const index of order) {
+//     if (total += index);
+//   }
+
+//   // Change code above this line
+//   return total;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+// console.log(calculateTotalPrice([]));
+
+
 /**
   |============================
   | TASK # 27
   |============================
 */
+
+// Виконай рефакторинг функції filterArray(numbers, value), замінивши цикл for на for...of.
+
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   const filteredNumbers = [];
+
+//   for (const number of numbers) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+
+//   return filteredNumbers;
+//   // Change code above this line
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+
 /**
   |============================
   | TASK # 28
   |============================
 */
+
+// Доповни вираз остачі від ділення таким чином, щоб код проходив тести.
+
+
+// const a = 3 % 3;
+// const b = 4 % 3;
+// const c = 11 % 8;
+// const d = 12 % 7;
+// const e = 8 % 6;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+
 /**
   |============================
   | TASK # 29
   |============================
 */
+
+// Напиши функцію getEvenNumbers(start, end), яка повертає масив усіх парних чисел від start до end. Парним вважається число, яке ділиться на 2 без остачі (10 % 2 === 0).
+
+
+// function getEvenNumbers(start, end) {
+//    // Change code below this line
+  
+//     let numbers = [];
+
+//   for (let i = start; i <= end; i += 1) {
+//     if (i % 2 === 0) {
+//       numbers.push(i);
+//     }
+
+//   }
+
+//   return numbers;
+//     // Change code above this line
+//   }
+
+//   console.log(getEvenNumbers(2, 5));
+//   console.log(getEvenNumbers(3, 11));
+//   console.log(getEvenNumbers(6, 12));
+//   console.log(getEvenNumbers(8, 8));
+//   console.log(getEvenNumbers(7, 7));
+
+
 /**
   |============================
   | TASK # 30
   |============================
 */
+
+// Доповни код таким чином, щоб у змінну number записувалося перше число від start до end, яке ділиться на 5 без остачі.
+
+
+// const start = 6;
+// const end = 27;
+// let number;
+
+// for (let i = start; i < end; i += 1) {
+//   if (i % 5 === 0) {
+//     number = i;
+//     break;
+//   }
+// }
+
+// console.log(number);
+
+
 /**
   |============================
   | TASK # 31
   |============================
 */
+
+// Виконай рефакторинг функції findNumber(start, end, divisor) таким чином, щоб вона:
+
+// повертала перше число від start до end, яке ділиться на divisor без остачі
+// не використала оператор break
+// не використала змінну number
+
+
+// function findNumber(start, end, divisor) {
+//   // Change code below this line
+
+//   for (let i = start; i < end; i += 1) {
+//     if (i % divisor === 0) {
+//       return i;
+//     }
+//   }
+
+//   // Change code above this line
+// }
+
+// console.log(findNumber(2, 6, 5));
+// console.log(findNumber(8, 17, 3));
+// console.log(findNumber(6, 9, 4));
+// console.log(findNumber(16, 35, 7));
+
+
 /**
   |============================
   | TASK # 32
   |============================
 */
+
+// Напиши функцію includes(array, value), яка робить те саме, що і метод масиву масив.includes(значення) - перевіряє, чи присутнє в масиві array значення value, повертаючи true, якщо присутнє, і false в іншому випадку.
+
+// При виконанні цього завдання в тілі функції includes() не можна використовувати метод масив.includes(значення).
+
+
+function includes(array, value) {
+  // Change code below this line
+
+  // Change code above this line
+}
+
+
+
+
+
+

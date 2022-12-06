9ca71ec297b6ac7d902a56bf84860293cc436e2c
Viktor Shadura <shaduravik@gmail.com>
2022-12-07 01:13:57 +0200

    task 2.32

--- js/js-block-2.js
+++ js/js-block-2.js
@@ -923,11 +923,14 @@
 function includes(array, value) {
   // Change code below this line
 
+  return array.includes(value);
+
   // Change code above this line
 }
 
-
-
-
-
-
+console.log(includes([1, 2, 3, 4, 5], 3));
+console.log(includes([1, 2, 3, 4, 5], 17));
+console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter"));
+console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus"));
+console.log(includes(["apple", "plum", "pear", "orange"], "plum"));
+console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
